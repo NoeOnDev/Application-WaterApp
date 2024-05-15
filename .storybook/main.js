6532156667931cbcd15962f3030b33628973c895
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-05-15 02:19:40 -0600

    feat: Update ToggleButton component to use yellow stroke color

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../src/", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",

bcd54d118410a2499e42e869094da3ed37d98415
NoeOnDev <alxg5516@gmail.com>
2024-05-16 00:24:25 -0600

    chore: Update .gitignore and LoginForm component

--- src/components/molecules/LoginForm/LoginForm.tsx
+++ src/components/molecules/LoginForm/LoginForm.tsx
@@ -1,14 +1,14 @@
-import React, {useState} from 'react'
-import {View} from 'react-native'
-import {ButtonAuth, Input} from '../../atoms'
+import React, {useState} from 'react';
+import {View} from 'react-native';
+import {ButtonAuth, Input} from '../../atoms';
 
 interface LoginFormProps {
-  onSubmit: (username: string, password: string) => void
+  onSubmit: (username: string, password: string) => void;
 }
 
 export const LoginForm: React.FC<LoginFormProps> = ({onSubmit}) => {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
   return (
     <View>
@@ -25,5 +25,5 @@ export const LoginForm: React.FC<LoginFormProps> = ({onSubmit}) => {
       />
       <ButtonAuth title="Login" onPress={() => onSubmit(username, password)} />
     </View>
-  )
-}
+  );
+};
--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -1,13 +1,15 @@
-import {View, Text} from 'react-native'
-import { LoginForm } from '../molecules'
+import {View, Text} from 'react-native';
+import {LoginForm} from '../molecules';
 
 export function LoginPage() {
-
   return (
     <View>
       <Text>Login Page</Text>
-      <LoginForm onSubmit={(username, password) => console
-        .log(`Username: ${username}, Password: ${password}`)} />
+      <LoginForm
+        onSubmit={(username, password) =>
+          console.log(`Username: ${username}, Password: ${password}`)
+        }
+      />
     </View>
-  )
+  );
 }

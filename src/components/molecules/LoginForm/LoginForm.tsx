633ffdcd2f2a59183b9a7bce4612db512334f56c
NoeOnDev <alxg5516@gmail.com>
2024-05-16 00:05:43 -0600

    feat: Add LoginForm component for login page

---
+++ src/components/molecules/LoginForm/LoginForm.tsx
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+import {View} from 'react-native';
+import {ButtonAuth, Input} from '../../atoms';
+
+interface LoginFormProps {
+  onSubmit: (username: string, password: string) => void;
+}
+
+export const LoginForm: React.FC<LoginFormProps> = ({onSubmit}) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <View>
+      <Input
+        value={username}
+        onChangeText={setUsername}
+        placeholder="Username"
+      />
+      <Input
+        value={password}
+        onChangeText={setPassword}
+        placeholder="Password"
+      />
+      <ButtonAuth title="Login" onPress={() => onSubmit(username, password)} />
+    </View>
+  );
+};
--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -1,14 +1,10 @@
-import {View, Text} from 'react-native'
-import {ButtonAuth} from '../atoms/ButtonAuth/ButtonAuth'
-import {Input} from '../atoms/inputAuth/InputAuth'
+import {View, Text} from 'react-native';
 
 export function LoginPage() {
+
   return (
     <View>
       <Text>Login Page</Text>
-      <Input value="" onChangeText={() => {}} placeholder="Username" />
-      <Input value="" onChangeText={() => {}} placeholder="Password" />
-      <ButtonAuth title="Login" onPress={() => {}} />
     </View>
-  )
+  );
 }

d138d873a69b279eca8eb13b5570d832a0d4de99
NoeOnDev <alxg5516@gmail.com>
2024-05-19 00:49:27 -0600

    style: Update LinkButton marginTop to 16 for better spacing

--- src/components/atoms/LinkButton/StylesLinkButton.tsx
+++ src/components/atoms/LinkButton/StylesLinkButton.tsx
@@ -3,7 +3,7 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
     text: {
       color: 'black',
-      marginTop: 12,
+      marginTop: 16,
       textAlign: 'center',
       fontSize: 18,
       fontWeight: '500',
